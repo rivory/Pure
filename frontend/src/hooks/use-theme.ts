13fc28639208f66bf9db4e466e586ed43ebc8380
Alex Lévy <alex.levy@relet-digital.com>
2025-02-24 22:09:17 +0100

    remove deprecated theme hook

--- frontend/src/hooks/use-theme.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-
-export function useTheme() {
-    const [theme, setTheme] = useState<'light' | 'dark'>(() => {
-        return (localStorage.getItem('theme') as 'light' | 'dark') || 'dark'
-    })
-
-    return theme
-} 
